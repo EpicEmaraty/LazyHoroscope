035f05c52c8c69083cc4b0fe5d24b783644c0fd1
epicemaraty <realnuaimi@mail.csuchico.edu>
2018-02-14 22:14:32 -0800

    Added the lucky number

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
   string first;
   string last;
+  int number;
   //get user input
   cout<<"Enter your first name: ";
   cin>> first;
@@ -16,7 +17,7 @@ int main()
   cin>> last;
   //tell fortune
   cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..." <<endl;
-
+  cout<<"Your lucky number is " <<first.length()<<endl;
   
   return 0;
 }
