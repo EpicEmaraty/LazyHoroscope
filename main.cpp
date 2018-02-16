9502c7209ae5da020f876da35c209ea72bd843fd
epicemaraty <realnuaimi@mail.csuchico.edu>
2018-02-15 20:28:45 -0800

    Completed

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ int main()
   string last;
   int number;
   //get user input
-  cout<<"Enter your first name: "<<endl;
+  cout<<"What is your first name?"<<endl;
   cin>> first;
-  cout<<"Enter your last name: "<<endl;
+  cout<<"What is your last name?"<<endl;
   cin>> last;
   //tell fortune
   cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..." <<endl;
-  cout<<"Your lucky number is " <<first.length()<<endl;
+  cout<<"your lucky number is " <<first.length()<<endl;
 
  if ( first[0]== 'a' || first[0]=='e' || first[0]== 'i' || first[0]== 'o' || first[0]== 'u' || first[0]== 'A' || first[0]== 'E' || first[0]== 'I' || first[0]== 'O' || first[0]== 'U')
 {
